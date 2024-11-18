5715dc8c117335929c3c826877e6d4d368f92228
navneet21bcs <navneet9876543@gmail.com>
2024-11-18 20:53:09 +0530

    url changed and secured

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://lms-project-lac.vercel.app",
     credentials:true
 }));
  
